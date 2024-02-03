b294726a00842215076f48838e63bad6f9bcc15a
Michael-Chacon <michaelchacon02@gmail.com>
2024-02-03 09:08:35 -0500

    style: :lipstick: Carrito de compras
    Creé el carrito de compras junto con su ventana modal que contiene el listado de todos los productos agregados al carrito de compras, funciona para desktop y mobil también creé el menú de navegación.
    
    @CampusLands
    commit SHA: a7bece9

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-window.show.addEventListener('click', () => {
-    window.dialog.showModal()
-})
+//  Descripcion del producto 
+window.show.addEventListener("click", () => {
+  window.dialog.showModal();
+});
 
-window.hide.addEventListener('click', () => {
-    window.dialog.close()
-})
+window.hide.addEventListener("click", () => {
+  window.dialog.close();
+});
+
+// Carrito de compras 
+
+window.mostrar.addEventListener("click", () => {
+  window.car.showModal();
+});
+
+window.cerrar.addEventListener("click", () => {
+  window.car.close();
+});
